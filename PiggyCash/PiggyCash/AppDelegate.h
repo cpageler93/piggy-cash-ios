8fb30adf23f37d9633b6a818680e4f1f5bae9200
Christoph Pageler <christoph.pageler@ewetel.net>
2017-01-15 13:46:15 +0100

    initial commit

---
+++ PiggyCash/PiggyCash/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PiggyCash
+//
+//  Created by Christoph Pageler on 15.01.2017.
+//  Copyright © 2017 Christoph Pageler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PiggyCash/PiggyCash/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PiggyCash
+//
+//  Created by Christoph Pageler on 15.01.2017.
+//  Copyright © 2017 Christoph Pageler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
